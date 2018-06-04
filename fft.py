b7d01e8cc207e3953d308aeae95d84a3c04a4939
itchencheng <itchencheng@gmail.com>
2018-06-05 00:38:33 +0800

    fft

--- fft.py
+++ fft.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 
 
 def main():
-	x = np.arange(40)
-	y = np.cos(0.2 * np.pi * x)
+    x = np.arange(40)
+    y = np.cos(0.2 * np.pi * x)
 	
-	y2 = np.fft.fft(y)
+    y2 = np.fft.fft(y)
 	
-	print(x)
+    print(x)
+    print(len(y))
+    print(len(y2))
 	
-	#plt.scatter(x, y, color="red")
-	plt.scatter(x, y2, color="green")
+    #plt.scatter(x, y, color="red")
+    plt.scatter(x, y2, color="green")
 	
-	plt.show()
+    plt.show()
 
 if __name__ == "__main__":
-	main()
+    main()
